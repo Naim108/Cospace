dde1da930ca5a898cc8c9eeb451c3a0d5ed074c9
Naim Hossain <nayeemhossain108@gmail.com>
2023-11-25 22:06:21 +0600

    date fixed

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,6 @@
 $(window).on('load',function(){
    $('#preloader').delay(1000).fadeOut(1000);
 });
-
-
-/* !navbar script */
-let openBtn=document.querySelector('.bi-list')
-let closeBtn=document.querySelector('.bi-x-lg')
-let menu=document.querySelector('.navbar__menu')
-
-openBtn.addEventListener('click',()=>{
-   openBtn.classList.toggle("hide")
-   closeBtn.classList.toggle("hide")
-   menu.classList.toggle("hidden")
-})
-closeBtn.addEventListener('click',()=>{
-    openBtn.classList.toggle("hide")
-    closeBtn.classList.toggle("hide")
-    menu.classList.toggle("hidden")
- })
-
  /* form js */
  $(document).ready(function(){
    $(".submit-btn").click(function(){
